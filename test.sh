7e6ab6032403bfd1cf9e5843bd821bfaf41da745
kuni_t <to.ku.development.kit@gmail.com>
2023-10-30 15:11:24 +0900

    Add + and - operators

--- test.sh
+++ test.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 assert() {
 
-   expected="$1"
-   input="$2"
+    expected="$1"
+    input="$2"
 
-   ./1cc "$input" > tmp.s || exit
-   gcc -static -o tmp tmp.s
-   ./tmp
+    ./1cc "$input" > tmp.s || exit
+    gcc -static -o tmp tmp.s
+    ./tmp
 
-   actual="$?"
+    actual="$?"
 
-   if [ "$actual" = "$expected" ]; then
-      echo "$input => $actual"
-   else
-      echo "$input => $expected expected, but got $actual"
-	exit 1
-   fi
+    if [ "$actual" = "$expected" ]; then
+        echo "$input => $actual"
+    else
+        echo "$input => $expected expected, but got $actual"
+        exit 1
+    fi
 }
 
 assert 0 0
 assert 42 42
+assert 21 '5+20-4'
 
 echo OK
